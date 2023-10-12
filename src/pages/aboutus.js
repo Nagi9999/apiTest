ba18e995e581058856c519eba89466638c11ca98
Naji <najielhakim9@gmail.com>
2023-10-13 00:25:39 +0300

    project setup

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,71 +1,75 @@
 "use client";
 import axios from "@/utils/axios";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import SwiperCore, { Navigation } from "swiper/core";
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 SwiperCore.use([Navigation]);
 
 export default function Page({ data }) {
   const swiperRef = useRef(null);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
 
   const goToNextSlide = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
-      swiperRef.current.swiper.slideNext();
     }
   };
 
   const goToPrevSlide = () => {
     if (swiperRef.current && swiperRef.current.swiper) {
-      swiperRef.current.swiper.slidePrev();
     }
   };
 
   return (
     <div>
-      <button onClick={goToPrevSlide} className="swiper-button-prev "></button>
+      <button onClick={goToPrevSlide} className="swiper-button-prev"></button>
       <button onClick={goToNextSlide} className="swiper-button-next"></button>
 
       <Swiper
         ref={swiperRef}
-        spaceBetween={50}
-        slidesPerView={1} // Initial slides per view for larger screens
+        spaceBetween={20}
+        slidesPerView={1}
+        centeredSlides={true}
         navigation={{
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         }}
-        // Add responsive breakpoints
         breakpoints={{
           600: {
-            // Slides per view on screens 768px and larger
             slidesPerView: 3,
           },
         }}
+        onSlideChange={(swiper) => {
+          console.log("slide change", swiper.activeIndex);
+          setActiveSlideIndex(swiper.activeIndex);
+        }}
       >
-        {data.data.map((item) => (
+        {data.data.map((item, index) => (
           <SwiperSlide key={item.title}>
-            <div className="2xl:container mx-auto sm:my-32 my-16 sm:px-20">
-              <div className="flex justify-between sm:mt-10 mt-4">
-                <div className="col-span-  rounded-md sm:mx-1 mx-3 text-justify items-center sm:px-6 px-3 sm:py-10 py-4 sm:my-0 my-5">
-                  <div href="/">
-                    <div className="relative overflow-hidden ">
-                      <img
-                        src={item.image}
-                        alt="Your Icon"
-                        className={`duration-300 ease-[cubic-bezier(0.25, 1, 0.5, 1)] w-auto h-auto my-[5px] `}
-                      />
-                    </div>
-                    <h3 className="font-bold sm:text-2xl text-xl sm:mt-4 text-white my-3">
-                      {item.title}
-                    </h3>
-                    <p className="sm:mt-4 mt-2 pr-4 sm:text-xl text-lg text-white sm:tracking-normal tracking-tighter">
-                      {item.text}
-                    </p>
+            <div className="flex justify-between sm:mt-10 mt-4">
+              <div className={`flex justify-center `}>
+                <div>
+                  <div className="relative">
+                    <img
+                      src={item.image}
+                      alt="Your Icon"
+                      className="sm:h-[400px]  "
+                    />
                   </div>
+                  {index === activeSlideIndex ? (
+                    <div className="m-8">
+                      <h2
+                        className={`font-bold sm:text-2xl text-xl sm:mt-4 text-white my-3 pr-4 `}
+                      >
+                        {item.title}
+                      </h2>
+                      <p className="sm:mt-4 mt-2 pr-4 sm:text-xl text-lg text-white sm:tracking-normal tracking-tighter">
+                        {item.text}
+                      </p>
+                    </div>
+                  ) : null}
                 </div>
               </div>
             </div>
