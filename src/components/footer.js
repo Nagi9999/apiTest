6ffd77941b1f5ac2742d51ad85d5935b8965341e
Naji <najielhakim9@gmail.com>
2023-10-18 16:41:48 +0300

    project setup

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,28 +1,21 @@
-"use client";
+"use client"
 import Link from "next/link";
-const Footer = () => {
+
+
+const Footer = (props) => {
   const menuItems = [
-    { title: "about us", path: "/" },
-    { title: "Inventory", path: "/" },
-    { title: "Special Offers", path: "/" },
-    { title: "contact us", path: "/" },
-  ];
-  const menuItems1 = [
-    { title: "dubai,UAE", path: "/" },
-    { title: "beirut,LEBANON", path: "/" },
-    { title: "paris,FRANCE", path: "/" },
-    { title: "cairo,EGYPT", path: "/" },
-    { title: "riyadh,KSA", path: "/" },
+    { title: "about us", path: "/aboutus" },
+    { title: "Inventory", path: "/inventory" },
+    { title: "Special Offers", path: "/specialOffers" },
+    { title: "contact us", path: "/contactus" },
   ];
 
   return (
-    <div className="custombackground ">
-      <div className="2xl:container mx-auto ">
-        <div className="  flex flex-col justify-center  text-white my-16 sm:pl-32 pl-12  ">
-          <h1 className="text-xl">sitemap</h1>
-          <ul
-            className={`flex  flex-col font-semibold  text-sm  my-4  sm:space-y-0 space-y-2 mt-6`}
-          >
+    <div className="bg-blue-900">
+      <div className="2xl:container mx-auto">
+        <div className="flex flex-col items-center text-center text-white my-16">
+          <h1 className="sm:text-3xl text-2xl font-bold">sitemap</h1>
+          <ul className={`flex flex-col font-semibold text-sm my-4 sm:space-y-2 space-y-2 mt-6`}>
             {menuItems.map((item, index) => (
               <li key={index}>
                 <Link href={item.path}>
@@ -35,25 +28,18 @@ const Footer = () => {
 
         <div className="flex sm:justify-center justify-around items-center sm:space-x-16 text-white my-16">
           <Link href="/">
- <div className="text-3xl font-bold">
-            <a href="/" className=" ">
-              <span className="text-blue-500">Auto</span>
-              <span className="text-orange-200">Vista</span>
-            </a>
-          </div>          </Link>
-          <a href="www.instagram.com" class="no-underline text-white">
-            <img
-              src="/favicon/instagram.png"
-              alt="Instagram"
-              class="sm:w-6 h-6"
-            />
+            <div className="text-3xl font-bold">
+              <a href="/" className=" ">
+                <span className="text-blue-500">Auto</span>
+                <span className="text-orange-500">Vista</span>
+              </a>
+            </div>
+          </Link>
+          <a href={props.socialLinks.en.Instagram} className="no-underline text-white">
+            <img src="/favicon/instagram.png" alt="Instagram" className="sm:w-6 h-6" />
           </a>
-          <a href="www.linkedin.com" class="no-underline text-white">
-            <img
-              src="/favicon/linkedin.png"
-              alt="LinkedIn"
-              class="w-6 h-6 "
-            />
+          <a href={ props.socialLinks.en.Linkedin} className="no-underline text-white">
+            <img src="/favicon/linkedin.png" alt="LinkedIn" className="w-6 h-6" />
           </a>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,18 @@ import Card from "@/components/card";
 import Footer from "@/components/footer";
 import axios from "@/utils/axios";
 
-export default function Home({data }) {
+export default function Home({data, data1 }) {
   console.log({data})
   return (
     
 <main className="  flex flex-col justify-center  ">  
     <Banner data={data} />
     <Card data={data}  />
-    <Footer/>
+    <Footer socialLinks={data1.data.social_media} />
 </main>  
 )
 }
+
 
 export async function getServerSideProps({ locale }) {
   let data;
@@ -28,5 +29,17 @@ export async function getServerSideProps({ locale }) {
       data = res.data;
     })
     .catch(console.error);
-  return { props: { data } };
+
+ let data1;
+    await axios
+        .get("/settings", {
+            headers: {
+                "Accept-Language": locale,
+            },
+        })
+        .then((res) => {
+            data1 = res.data;
+        })
+        .catch(console.error);
+    return { props: { data ,data1} };
 }
