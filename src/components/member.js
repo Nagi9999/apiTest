16a4d332b76a263bd842abf66c98da565d3a0c90
Naji <najielhakim9@gmail.com>
2023-10-24 13:10:10 +0300

    project

--- src/components/member.js
+++ src/components/member.js
@@ -97,6 +97,7 @@ const Member = () => {
         });
       };
 
+    // Call the spinImages function when the component mounts
     spinImages();
     animateText();
     animateBorder();
@@ -104,35 +105,35 @@ const Member = () => {
   
 
   return (
-    <div className="  justify-center pt-20 my-20">
-    <div className="flex flex-wrap justify-between sm:space-x-16 space-x-4 sm:space-y-16 space-y-4 my-8">
-      {Data.map((item) => {
-        return (
-          <div
-            key={item.id}
-            className="w-[25%] flex-col px-8 sm:px-8 py-4 sm:py-8 rounded-md text-justify text-white text-2xl max-w-[300px]"
-          >
-            <div className="box-border border-orange-500 border-2 jus rounded-full p-4 flex flex-col items-center justify-center border-circle">
-              <img
-                className="w-[200px] h-[200px] rounded-full object-fill spin-image"
-                src={item.src}
-                alt="This is a card"
-              />
-            </div>
-
-            <div className="flex ml-[50%]">
-              <div className="vertical-line scale-text"></div>
-              <div className="pl-2 pb-16 scale-text">
-                <h3>{item.name}</h3>
-                <p className="font-bold">{item.category}</p>
+    <div className="flex justify-center pt-20 my-20">
+      <div className="grid sm:grid-cols-4 grid-cols-1 justify-between sm:space-x-16 space-x-4  sm:space-y-16 space-y-4 my-8">
+        {Data.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="px-8 sm:px-8 py-4 sm:py-8  rounded-md text-justify text-white text-2xl max-w-[300px]"
+            >
+              <div className="box-border border-orange-500 border-2 jus rounded-full p-4 flex flex-col items-center justify-center border-circle">
+                <img
+                  className="w-[200px] h-[200px] rounded-full object-fill spin-image"
+                  src={item.src}
+                  alt="This is a card"
+                />
               </div>
+
+              <div className="flex ml-[50%]  ">
+                <div className="vertical-line scale-text"></div>
+                <div className="pl-2 pb-16 scale-text">
+                  <h3>{item.name}</h3>
+                  <p className="font-bold">{item.category}</p>
+                </div>
+              </div>
+              <div className="ml-[45%] w-7 h-7 rounded-full bg-orange-500 scale-text" />
             </div>
-            <div className="ml-[45%] w-7 h-7 rounded-full bg-orange-500 scale-text" />
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
-  </div>
   );
 };
 
