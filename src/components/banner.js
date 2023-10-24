3408da3a3f71ff322a9298210533e9fec40f4b2e
Naji <najielhakim9@gmail.com>
2023-10-24 08:36:42 +0300

    project setup

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,4 +1,21 @@
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+
 const Banner = (props) => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const textElement = textRef.current;
+    if (textElement) {
+      gsap.from(textElement, {
+        y: '100%',
+        opacity: 0,
+        duration: 3,
+        ease: 'power4.out',
+      });
+    }
+  }, []);
+
   return (
     <div className="relative sm:mt-0 mt-16">
       <div>
@@ -9,7 +26,10 @@ const Banner = (props) => {
         />
       </div>
       <div className="2xl:container mx-auto absolute inset-0 flex flex-col justify-center items-center mt-4">
-        <h1 className="sm:text-7xl text-center sm:px-60 px-16 items-center sm:font-light font-semibold">
+        <h1
+          ref={textRef}
+          className="sm:text-7xl text-center sm:px-60 px-16 items-center sm:font-light font-semibold"
+        >
           <span className="progressive-text">
             Upgrade your life with our sleek, reliable cars. Experience luxury,
             power, and style. Unleash adventure, make memories, and embrace
--- src/pages/inventory.js
+++ src/pages/inventory.js
@@ -1,40 +1,62 @@
-"use client"
-import WallPaper from "@/components/wallpaper";
-import axios from "@/utils/axios";
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import WallPaper from '@/components/wallpaper';
+import axios from '@/utils/axios';
 
-const Inventory = ({data}) => {
+const Inventory = ({ data }) => {
+  const textRefs = useRef([]);
 
+  useEffect(() => {
+    // Define the GSAP animation
+    const animateText = (textRef) => {
+      gsap.fromTo(
+        textRef,
+        { x: '100%', scale: 0 },
+        { x: '0%', scale: 1, duration: 3, ease: 'power4.out' }
+      );
+    };
 
-    return ( 
-        <main className="2xl:container mx-auto">
-        <WallPaper imageSrc={data.data[0].image} textPosition="left" imagePosition="right">
-          <h1 className="sm:text-4xl text-lg sm:font-semibold">{data.data[0].title}</h1>
-          <p className="font-semibold sm:text-lg text-[8px]">{data.data[0].text}</p>
+    textRefs.current.forEach((textRef) => {
+      animateText(textRef);
+    });
+  }, []);
+
+  return (
+    <main className="2xl:container mx-auto">
+      {data.data.map((item, index) => (
+        <WallPaper
+          key={index}
+          imageSrc={item.image}
+          textPosition={index % 2 === 0 ? 'left' : 'right'}
+          imagePosition={index % 2 === 0 ? 'right' : 'left'}
+        >
+          <div
+            ref={(el) => (textRefs.current[index] = el)}
+            style={{ transform: 'translate(0%, -50%)' }}
+          >
+            <h1 className="sm:text-4xl text-lg sm:font-semibold">{item.title}</h1>
+            <p className="font-semibold sm:text-lg text-[8px]">{item.text}</p>
+          </div>
         </WallPaper>
-        <WallPaper imageSrc={data.data[1].image} textPosition="right" imagePosition="left">
-        <h1 className="sm:text-4xl text-lg sm:font-semibold">{data.data[1].title}</h1>
-          <p className="font-semibold sm:text-lg text-[8px]">{data.data[1].text}</p>
-        </WallPaper>
-      </main>
-     );
-}
- 
+      ))}
+    </main>
+  );
+};
+
 export default Inventory;
 
-
 export async function getServerSideProps({ locale }) {
-    try {
-      const response = await axios.get("/projects", {
-        headers: {
-          "Accept-Language": locale,
-        },
-      });
-      const data = response.data;
-  
-      return { props: { data } };
-    } catch (error) {
-      console.error(error);
-      return { props: { data: [] } }; // Provide an empty array as a fallback
-    }
+  try {
+    const response = await axios.get('/projects', {
+      headers: {
+        'Accept-Language': locale,
+      },
+    });
+    const data = response.data;
+
+    return { props: { data } };
+  } catch (error) {
+    console.error(error);
+    return { props: { data: [] } }; // Provide an empty array as a fallback
   }
-  
+}
